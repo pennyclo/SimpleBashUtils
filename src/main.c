5bca606bbba8070a75cbc535e22192a97eccc889
Pennycandy Louetta <pennyclo@student.21-school.ru>
2024-07-24 00:33:42 +0500

    fix function: parser

--- src/main.c
+++ src/main.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
 
-#include "../cat_util/cat.h"
+#include "cat_util/cat.h"
 
-arguments *parcer(int argc, char** argv);
-void reader(char** argv);
+char** parcer(int argc, char** argv, int* num_files);
+void reader(char* file_path);
+char* strdup(const char* str);
 
 int main(int argc, char** argv) {
+    int num_files;
+    char** file = parcer(argc, argv, &num_files);
+
+    for(int i = 0; i < num_files; i++) {
+        reader(file[i]);
+    }
     
+    for(int i = 0; i < num_files; i++) {
+        free(file[i]);
+    }
+
+    free(file);
 
     return 0;
 }
 
-arguments *parcer(int argc, char** argv) {
+char** parcer(int argc, char** argv, int* num_files) {
     arguments argument;
     int opt;
+    *num_files = 0;
+    char **file_paths = NULL;
 
-    while(opt = getopt(argc, argv, "beEnstTvA")) {
+    while((opt = getopt(argc, argv, "b:e:E:n:s:t:T:v:A:")) != -1) {
         switch (opt) {
             case 'b':
                 argument.b = 1;
@@ -54,12 +69,61 @@ arguments *parcer(int argc, char** argv) {
                 fprintf(stderr, "Unknown options <-%c>", opt); // if this option does not exist
                 exit(1);
         }
+
+        char **new_file_path = realloc(file_paths, sizeof(char *) * (*num_files + 1));
+        if(!new_file_path) {
+            printf("Error allocated file path");
+            exit(1);
+        }
+
+        file_paths = new_file_path;
+
+        file_paths[*num_files] = strdup(optarg);
+        if(!file_paths[*num_files]) {
+            printf("Error allocated new file path");
+            exit(1);
+        }
+
+        *num_files += 1;
     }
 
-    return &argument;
+    return file_paths;
 }
 
-void reader(char** argv) {
+void reader(char* file_path) {
+    FILE *file = fopen(file_path, "r");
 
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* buffer = (char *) malloc(file_size + 1);
+    if(!buffer) {
+        printf("Error allocated buffer");
+        fclose(file);
+        exit(1);
+    } else {
+        size_t bytes_read = fread(buffer, 1, file_size, file);
+        if((long)bytes_read != file_size) {
+            printf("Error reading files");
+            free(buffer);
+            fclose(file);
+            exit(1);
+        } else {
+            buffer[file_size] = '\0';
+            printf("%s", buffer);
+            free(buffer);
+            fclose(file);
+        }
+    }
 }
 
+char* strdup(const char* str) {
+    size_t length = strlen(str) + 1;
+    char* new_str = malloc(length);
+    if (new_str) {
+        memcpy(new_str, str, length);
+    }
+    
+    return new_str;
+}
